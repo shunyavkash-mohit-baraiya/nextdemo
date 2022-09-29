088fda1ba7ecb329c93c914645a75592a5a02024
mohitbaraiya <mohit.baraiya@shunyavkash.com>
2022-09-29 18:05:11 +0530

    extra content removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -135,10 +135,7 @@ const IndexPage = () => {
           — you just made a Gatsby site! 🎉🎉🎉
         </span>
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
+
       <ul style={listStyles}>
         <li style={docLinkStyle}>
           <a
